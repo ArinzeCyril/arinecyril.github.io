d408656993d0193bd30cb3da3a856fbbb3185c00
Arinze Cyril <61941210+ArinzeCyril@users.noreply.github.com>
2022-07-15 05:57:18 +0100

    new commit

--- src/Main.js
+++ src/Main.js
@@ -2,26 +2,26 @@ export default function Main() {
   return (
     <main>
         <section>
-            <h2>About</h2>
+            <h2>About Me</h2>
             <p>Hello! I am a Frontend Developer and I enjoy 
               creating things that lives on the internet. I am particularly 
               interested in building high performing web pages.
           </p>
           <p>
-            I create responsive websites that are fast, easy to use and built with best
+            I create responsive websites that are fast, easy to use and are built with the best
             practices. Currently, the main area of my expertise is frontend development,
             though I do a little bit of backend and database.
         </p>
-          <p> My interest in web development started back in 2016 and Since then, 
-              I have become much better at it and been able to complete a number of
-              projects, work with a team of developers.
+          <p> My interest in web development started back in 2016 and since then, 
+              I have become much better at it and I have been able to complete a number of
+              projects and also worked with a team of developers.
           </p>
         </section>
 
         <section>
             <h2>Intersts</h2>
-            <p>Good music, tech & coding, digital marketing, animation, 
-              computer troubleshooting, forex, entrepreneur, travel geek.
+            <p>Good Music, Tech & Coding, Digital Marketing, Animation, 
+              Computer Troubleshooting, Torex, Entrepreneurship, Travel Geek.
             </p>
         </section>
     </main>
